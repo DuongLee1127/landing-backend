72f3749c8ff6d8fc52a6c44a1058eb933741d141
DuongLee1127 <boibu0127@gmail.com>
2025-10-27 13:54:24 +0700

    27/10/2025

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,37 +2,115 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
+use function PHPUnit\Framework\isEmpty;
+
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-        $user = User::where('email', $request->email)->first();
+        try {
+            $validate = $request->validate(
+                [
+                    'email' => 'required|email',
+                    'password' => 'required|min:6|max:16',
+                ],
+                [
+                    'email.required' => "Email không được bỏ trống",
+                    'email.email' => "Không đúng định dạng email",
+                    'password.required' => "Mật khẩu không được bỏ trống",
+                    'password.min' => "Mật khẩu ít nhất 6 ký tự"
+                ]
+            );
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password, [])) {
+                return response()->json([
+                    'message' => "Lỗi đăng nhập"
+                ], 401);
+            }
+
+            $token = $user->createToken('api_token')->plainTextToken;
+
             return response()->json([
-                'message' => 'Email hoặc mật khẩu không đúng'
+                'message' => 'Đăng nhập thành công',
+                'token' => $token,
+                'user' => $user
+            ]);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => $e->errors(),
+                'errors' => $e->errors(),
             ], 401);
         }
-        $token = $user->createToken('api_token')->plainTextToken;
 
-        return response()->json([
-            'message' => 'Đăng nhập thành công',
-            'token' => $token,
-            'user' => $user
-        ]);
+
+    }
+
+    public function register(Request $request)
+    {
+        // return [
+        //     'email' => $request->email,
+        //     'name' => $request->name,
+        //     'password' => $request->password
+        // ];
+
+        try {
+            $validate = $request->validate(
+                [
+                    'email' => 'required|email',
+                    'password' => 'required|min:6|max:16|same:repassword',
+                    'repassword' => 'required'
+                ],
+                [
+                    'email.required' => "Email không được bỏ trống",
+                    'email.email' => "Không đúng định dạng email",
+                    'password.required' => "Mật khẩu không được bỏ trống",
+                    'password.min' => "Mật khẩu ít nhất 6 ký tự",
+                    'repassword.required' => "Mật khẩu nhập lại không được bỏ trống",
+                    'password.same' => "Nhập lại mật khẩu không trùng",
+                ]
+            );
+
+            User::create([
+                'email' => $request->email,
+                'name' => $request->name,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'message' => 'Đăng ký tài khoản thành công'
+            ], 200);
+
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => $e->errors(),
+                'errors' => $e->errors(),
+            ], 401);
+        }
+
+
+
     }
 
     public function user(Request $request)
     {
         return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => "Đăng xuất"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
