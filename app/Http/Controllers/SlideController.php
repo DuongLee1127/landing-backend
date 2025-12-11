bb5b294958489f2561c4385f831fb3efbeee335c
DuongLee1127 <boibu0127@gmail.com>
2025-12-11 16:09:58 +0700

    2

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -45,7 +45,7 @@ class SlideController extends Controller
         }
 
         $path = $request->file('image')->store('uploads', 'public');
-        dd($request->file('image'));
+        // dd($request->file('image'));
 
         $url = asset('storage/' . $path);
         $slide = Slide::create(['path' => $path, 'url' => $url, 'user_id' => $request->user()->id]);
